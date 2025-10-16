fffdb0c71314d8ab057b646b0b6e03146b99763d
Renukaradhya Odeyar C G <odeyarrenukaradhya@gmail.com>
2025-10-16 22:04:21 +0530

    Add files via upload

---
+++ classOne.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Fruit extends React.Component {
+  render() {
+    return <h2>Hello this is a class component</h2>;
+  }
+}
+export default Fruit;//if export is not return wecan't share the file
+
+
